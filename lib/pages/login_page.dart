f2a24b28a8374c6ae7af5a55bde18545a4e5585f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-07-03 11:56:00 +0900

    updated loginpage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,27 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "lib/assets/logo.png",
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            SizedBox(height: 25),
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            SizedBox(height: 25),
+            TextField(),
+          ],
+        ),
+      ),
     );
   }
 }
